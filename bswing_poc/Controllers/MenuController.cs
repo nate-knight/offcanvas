aafd4733a574d2d4894fa4b8fea3926890d9586e
Nate Knight <nateknight@gmail.com>
2014-06-06 15:54:17 -0500

    Update MenuController.cs

--- bswing_poc/Controllers/MenuController.cs
+++ bswing_poc/Controllers/MenuController.cs
@@ -1,34 +1,45 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
 using bswing_poc.Models;
+using Newtonsoft.Json;
 
 
 namespace bswing_poc.Controllers
 {
     public class MenuController : ApiController
     {
-        MenuItem[] menuItems = new MenuItem[] 
-        { 
-            new MenuItem { Title = "People" }, 
-            new MenuItem { Title = "Projects" }, 
-            new MenuItem { Title = "Teams" }
-        };
+        //MenuItem[] menuItems = new MenuItem[] 
+        //{ 
+        //    new MenuItem { menuTitle = "People", submenus = [new MenuItem{menuTitle = "This Week"}, new MenuItem{menuTitle = "Next Week"}] }, 
+        //    new MenuItem { menuTitle = "Projects" }, 
+        //    new MenuItem { menuTitle = "Teams" }
+        //};
+      
 
         // GET api/menu
-        public IEnumerable<MenuItem> Get()
+        public List<MenuItem> Get()
         {
-            return menuItems;
+            List<MenuItem> items = new List<MenuItem>();
+
+            using (StreamReader r = new StreamReader(System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_Data/menu.json")))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<MenuItem>>(json);
+            }
+
+            return items;
         }
 
        
 
-//public JsonResult Get()
-  //      {
+        //public JsonResult Get()
+        //      {
             //var items = new List<Menu>();
 
             //movies.Add(new { Title = "Ghostbusters", Genre = "Comedy", Year = 1984 });
