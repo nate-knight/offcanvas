165e4bb37a2de18a73dd646ffa174aa67a00b71e
Nate Knight <nateknight@gmail.com>
2014-06-06 16:09:17 -0500

    Update app.js

--- bswing_poc/Scripts/app.js
+++ bswing_poc/Scripts/app.js
@@ -3,14 +3,14 @@ var app = angular.module('app', []);
 
 app.controller('poc', function ($scope, $http) {
    
-    $scope.menuItems = [{ "menuTitle": "People", "submenus": [{ "menuTitle": "This Week", "submenus": [{ "menuTitle": "Monday", "submenus": null }, { "menuTitle": "Tuesday", "submenus": null }] }, { "menuTitle": "This Month", "submenus": [{ "menuTitle": "First Week", "submenus": null }, { "menuTitle": "Second Week", "submenus": null }] }, { "menuTitle": "Over/Under Report", "submenus": null }, { "menuTitle": "By Person", "submenus": null }] }, { "menuTitle": "Projects", "submenus": null }, { "menuTitle": "Teams", "submenus": null }, { "menuTitle": "Reports", "submenus": null }, { "menuTitle": "TimeClock", "submenus": null }];
+    $scope.menuItemsa = [{ "menuTitle": "People", "submenus": [{ "menuTitle": "This Week", "submenus": [{ "menuTitle": "Monday", "submenus": null }, { "menuTitle": "Tuesday", "submenus": null }] }, { "menuTitle": "This Month", "submenus": [{ "menuTitle": "First Week", "submenus": null }, { "menuTitle": "Second Week", "submenus": null }] }, { "menuTitle": "Over/Under Report", "submenus": null }, { "menuTitle": "By Person", "submenus": null }] }, { "menuTitle": "Projects", "submenus": null }, { "menuTitle": "Teams", "submenus": null }, { "menuTitle": "Reports", "submenus": null }, { "menuTitle": "TimeClock", "submenus": null }];
 
-    $http.get({
-        url:'/api/menu',
-        method: "GET",
-        params: { user_id: "nate" }
+    $http({
+        method: 'GET',
+        url: 'api/menu',
+        params: { user: 'nate' }
     }).success(function (data) {
-        $scope.menuItems2 = data;
+        $scope.menuItems = data;
     });
 
 
